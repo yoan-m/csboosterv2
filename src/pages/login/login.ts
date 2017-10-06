129c8da43b121228b5d3ca76b7c95ab8c96cb568
murciano <y.murciano@systel-sa.com>
2017-10-06 16:52:05 +0200

    login

---
+++ src/pages/login/login.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { 
+  IonicPage, 
+  NavController, 
+  LoadingController, 
+  Loading, 
+  AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
+import { EmailValidator } from '../../validators/email';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  public loginForm:FormGroup;
+  public loading:Loading;
+
+  constructor(public navCtrl: NavController, public authData: AuthProvider, 
+    public formBuilder: FormBuilder, public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController) {
+
+      this.loginForm = formBuilder.group({
+        email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+        password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+      });
+  }
+
+  loginUser(){
+    if (!this.loginForm.valid){
+      console.log(this.loginForm.value);
+    } else {
+      this.authData.loginUser(this.loginForm.value.email, this.loginForm.value.password)
+      .then( authData => {
+        this.navCtrl.setRoot('HomePage');
+      }, error => {
+        this.loading.dismiss().then( () => {
+          let alert = this.alertCtrl.create({
+            message: error.message,
+            buttons: [
+              {
+                text: "Ok",
+                role: 'cancel'
+              }
+            ]
+          });
+          alert.present();
+        });
+      });
+
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      this.loading.present();
+    }
+  }
+
+  goToResetPassword(){
+    this.navCtrl.push('ResetPasswordPage');
+  }
+
+  createAccount(){
+    this.navCtrl.push('SignupPage');
+  }
+
+}
